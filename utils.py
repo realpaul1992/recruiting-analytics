c3c2788d40e9260c29672c6c1d9a2cbb920963d6
realpaul1992 <patellipaolod@gmail.com>
2025-01-04 19:04:38 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,10 +1,20 @@
 # utils.py
 
-from datetime import datetime
+from datetime import datetime, date
+import pandas as pd
 
 def format_date_display(date_obj):
-    """Formatta una data in formato DD/MM/YYYY. Restituisce una stringa vuota se None."""
-    if date_obj:
+    """Formatta una data in formato DD/MM/YYYY. Restituisce una stringa vuota se None o non valida."""
+    if pd.isnull(date_obj):
+        return ""
+    if isinstance(date_obj, str):
+        # Tentativo di parsing della stringa assumendo formato YYYY-MM-DD
+        try:
+            date_parsed = datetime.strptime(date_obj, '%Y-%m-%d').date()
+            return date_parsed.strftime('%d/%m/%Y')
+        except ValueError:
+            return ""
+    if isinstance(date_obj, (datetime, date)):
         return date_obj.strftime('%d/%m/%Y')
     return ""
 
